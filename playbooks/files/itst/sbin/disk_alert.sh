2f56098363130c1d66a0d77338f92fb3416efaf4
Bo Morgan <morganbo85@gmail.com>
2024-12-11 16:17:21 -0600

    Update

--- playbooks/files/itst/sbin/disk_alert.sh
+++ playbooks/files/itst/sbin/disk_alert.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mingigs=10
-avail=$(df -BG /mnt/backup | awk 'NR==2 {print $4+0}')  # Extract available GB as an integer
-topicurl=198.58.105.88:8080/itst
+mingigs=5
+avail=$(df -BG / | awk 'NR==2 {print $4+0}')  # Extract available GB as an integer
+topicurl=44.217.53.30/itst_aws
 
 if (( avail < mingigs )); then
   curl \
-    -d "Only $avail GB available on the /mnt/backup disk. Better clean that up." \
+    -d "Only $avail GB available on the / disk. Better clean that up." \
     -H "Title: Low disk space alert on $(hostname)" \
     -H "Priority: high" \
     -H "Tags: warning,cd" \
--- playbooks/files/usr/local/bin/ntfy-ssh-login.sh
+++ playbooks/files/usr/local/bin/ntfy-ssh-login.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOPIC_URL=198.58.105.88:8080/itst
+TOPIC_URL=44.217.53.30/itst_aws
 
 if [ "${PAM_TYPE}" = "open_session" ]; then
   curl -H tags:warning -H prio:high -d "SSH login to $(hostname): ${PAM_USER} from ${PAM_RHOST}" "${TOPIC_URL}"
